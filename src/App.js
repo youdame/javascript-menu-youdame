2607cfff011730898d048cde954c2e9adfc8ac1d
youdame <112458620+youdame@users.noreply.github.com>
2024-12-13 16:02:57 +0900

    refactor: 변수명을 보다 명확하게 변경

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import { Random } from '@woowacourse/mission-utils';
 import InputView from './view/InputView.js';
 import OutputView from './view/OutputView.js';
-import validateNameInput from './validation/validateNameInput.js';
 import { CATEGORIES, MENU } from './constant/index.js';
 
 class App {
@@ -10,16 +9,23 @@ class App {
   category = [];
 
   /** @type {Record<string,string[]>}} */
-  coachesInfo = {};
+  coachesNotEatFoodInfo = {};
 
   /** @type {Record<string,string[]>}} */
   coachesMenu = {};
 
-  async getNotFoods() {
+  async getNotEatFood() {
     /// 못먹는 메뉴가 메뉴 리스트에 없는 거라면 있는 것만 해달라고 해야하지 않을까?
-    for (const name of Object.keys(this.coachesInfo)) {
-      const notFood = await InputView.readUserInput(`${name}(이)가 못 먹는 메뉴를 입력해 주세요.\n`);
-      this.coachesInfo[name].push(notFood);
+    for (const name of Object.keys(this.coachesNotEatFoodInfo)) {
+      const notEatFood = await InputView.readUserInput(`${name}(이)가 못 먹는 메뉴를 입력해 주세요.\n`);
+      this.coachesNotEatFoodInfo[name].push(notEatFood);
+    }
+  }
+
+  putCoachesName(coachesName) {
+    for (const name of coachesName) {
+      this.coachesNotEatFoodInfo[name] = [];
+      this.coachesMenu[name] = [];
     }
   }
 
@@ -27,30 +33,32 @@ class App {
     // 첫 출력
     this.printStartMessage();
 
-    // 코치 이름 받기
     const coachesName = await InputView.getCoachesName();
+    this.putCoachesName(coachesName);
 
-    for (const name of coachesName) {
-      this.coachesInfo[name] = [];
-      this.coachesMenu[name] = [];
-    }
-
-    await this.getNotFoods();
+    await this.getNotEatFood();
 
     this.printResultMessage();
+  }
+
+  printResultMessage() {
+    OutputView.printMessage('\n');
+    OutputView.printMessage('메뉴 추천 결과입니다.\n');
+    OutputView.printMessage('[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]');
+
+    this.selectCategory();
+    this.printSelectedCategory();
+    this.recommendMenus();
+    this.printRecommendedMenu();
+
+    OutputView.printMessage('추천을 완료했습니다.\n');
   }
 
   printStartMessage() {
     OutputView.printMessage('점심 메뉴 추천을 시작합니다.');
   }
 
-  printResultMessage() {
-    // 여기부터 출력임
-    OutputView.printMessage('\n');
-    OutputView.printMessage('메뉴 추천 결과입니다.\n');
-    OutputView.printMessage('[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]');
-
-    // 카테고리 선정
+  selectCategory() {
     for (let i = 1; i <= 5; i++) {
       let randomNumber = Random.pickNumberInRange(1, 5);
 
@@ -62,28 +70,31 @@ class App {
 
       this.category.push(CATEGORIES[randomNumber]);
     }
+  }
 
-    OutputView.printMessage(`[ 카테고리 | ${this.category[0]} | ${this.category[1]} | ${this.category[2]} | ${this.category[3]} | ${this.category[4]} ]`);
-
-    // 메뉴 넣고
-    for (const coach of Object.keys(this.coachesInfo)) {
+  recommendMenus() {
+    for (const coach of Object.keys(this.coachesNotEatFoodInfo)) {
       for (let i = 0; i <= 4; i++) {
-        this.coachesMenu[coach].push(this.getMenu(coach, i));
+        const recommendedMenu = this.recommendMenu(coach, i);
+        this.coachesMenu[coach].push(recommendedMenu);
       }
     }
+  }
 
-    // 메뉴 출력
-    for (const coach of Object.keys(this.coachesInfo)) {
+  printSelectedCategory() {
+    OutputView.printMessage(`[ 카테고리 | ${this.category[0]} | ${this.category[1]} | ${this.category[2]} | ${this.category[3]} | ${this.category[4]} ]`);
+  }
+
+  printRecommendedMenu() {
+    for (const coach of Object.keys(this.coachesNotEatFoodInfo)) {
       OutputView.printMessage(
         `[ ${coach} | ${this.coachesMenu[coach][0]} | ${this.coachesMenu[coach][1]} | ${this.coachesMenu[coach][2]} | ${this.coachesMenu[coach][3]} | ${this.coachesMenu[coach][4]} ]`,
       );
     }
-
-    OutputView.printMessage('추천을 완료했습니다.\n');
   }
 
-  getMenu(coach, index) {
-    const notFood = this.coachesInfo[coach];
+  recommendMenu(coach, index) {
+    const notFood = this.coachesNotEatFoodInfo[coach];
 
     while (true) {
       const currentCategory = this.category[index];
